0c892a10d5a14fb975937251ce69c5929e7e4729
alexandrejoao <xandybrown.aj@gmail.com>
2022-06-08 21:41:54 -0300

    add express no projeto

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT|| 4000;
+
+app.use(express.static(__dirname + '/'));
+
+app.listen(port, () => {
+    console.info(`\n Servidor Rodando na porta: ${port}\n`);
+});
